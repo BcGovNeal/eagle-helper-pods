670b7d91ac53e5d2504e6ec2c906a10562c60651
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2020-11-03 10:37:10 -0800

    Add rolebindings template

--- openshift/jenkins/openshift/setup_jenkins.sh
+++ openshift/jenkins/openshift/setup_jenkins.sh
@@ -4,6 +4,8 @@ oc -n $tools process -f "$templates_url/secrets.yaml" -p GH_USERNAME=$gh_usernam
 oc -n $tools process -f "$templates_url/ns-config.yaml" -p DEV=$dev -p TEST=$test -p PROD=$prod -p TOOLS=$tools | oc  -n $tools create -f -
 oc -n $tools process -f "$templates_url/jobs-config.yaml" -p REPO_OWNER=$repo_owner -p REPO_NAME_ADMIN=$repo_name_admin -p REPO_NAME_PUBLIC=$repo_name_public -p REPO_NAME_API=$repo_name_api -p APP_NAME=$app_name -p APP_DOMAIN=$app_domain | oc -n $tools create -f -
 
+oc -n $tools process -f "$templates_url/image-pull-rb.yaml" -p DEV=$dev -p TEST=$test -p PROD=$prod | oc  -n $tools create -f -
+
 oc -n $tools process -f "$templates_url/build-master.yaml" -p NAME=jenkins -p SUFFIX=-prod -p VERSION=prod-1.0.0 -p SOURCE_REPOSITORY_URL=$tools_repo_url -p SOURCE_REPOSITORY_REF=$tools_repo_ref -o yaml | oc -n $tools create -f -
 
 oc -n $tools process -f "$templates_url/build-slave.yaml" -p NAME=jenkins -p SUFFIX=-prod -p VERSION=prod-1.0.0 -p SLAVE_NAME=main -p SOURCE_IMAGE_STREAM_TAG=jenkins:prod-1.0.0 -o yaml | oc -n $tools create -f -
